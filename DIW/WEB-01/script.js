938c364f53b6617c86c12eeda89941ce635a5790
RamonRieraDev <ramonrieradev@gmail.com>
2024-10-10 17:07:03 +0200

    BACKUP

--- DIW/WEB-01/script.js
+++ DIW/WEB-01/script.js
@@ -63,15 +63,15 @@ const productos = [
     },
     {
       id:2,
-      titulo: "Noticia 2",
-      imagen: "noticias/noticia2.jpg",
-      contenido: "Contenido de la noticia 2"
+      titulo: "Ableton Push 3: Live sin ordenador en hardware 100% autónomo",
+      imagen: "imagenes/push3.jpg",
+      contenido: "El futuro ha llegado. Enorme salto el que da Push 3 en cualquiera de sus dos variantes. Sumando únicamente los pads 3D para interpretación MPE, el interfaz audio que incorpora, y su uso como controlador, diría que el precio mejora (aunque sea más alto) el de Push 2. La versión 'standalone' dobla el desembolso para quienes quieran usar Live sin ordenador, pero a cambio permite disponer del sistema comercial más potente que conocemos en este tipo de dispositivos, disfrutando de un Live sin restricciones en un equipo 100% autónomo y tremendamente versátil."
     },
     {
       id:3,
-      titulo: "Noticia 3",
-      imagen: "noticias/noticia3.jpg",
-      contenido: "Contenido de la noticia 3"  
+      titulo: "Review de Arturia Minilab 3, acertada renovación para un completo controlador MIDI",
+      imagen: "imagenes/minilab3.jpg",
+      contenido: "La experiencia de Arturia en controladores es ya dilatada, y ha permitido concebir una combinación que veo particularmente interesante en sus resultados, dentro de los de tipo 25 mini teclas. Y con un 'hard' cuidado en el que las teclas son decentes, cosa que no siempre sucede. Hace muy bien lo que hace y lo que hace es mucho. Para conseguir más, hay que ir a más espacio y a más presupuesto."  
     }
   ];
 
