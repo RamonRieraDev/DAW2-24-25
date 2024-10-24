4d611a8d6a2e13a2c3261b69d3c2eceef96dd964
RamonRieraDev <ramonrieradev@gmail.com>
2024-10-24 19:25:20 +0200

    BACKUP

---
+++ DWS/C#/proyectoPrueba/proyectoPrueba/helloWorld.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! XDDDD");
+
+// Otra línea
+Console.Write("wow\n");
+
+// Operaciones
+int num1 = 10;
+int num2 = 20;
+int resultado = num1 + num2;
+Console.WriteLine("El resultado de sumar " + num1 + " + " + num2 + " es " + resultado + "\n");
+
+Console.WriteLine("Escribe un número");
+string num3 = Console.ReadLine();
+
+Console.WriteLine("El número introducido es " + num3);
